ec57870ea14bb8140ca734d896021bc40a781308
Sheeju Alex <sheeju@exceleron.com>
2014-09-02 11:40:58 +0530

    bulk delete it works with PgLog

--- t/lib/PgLogTest/Schema.pm
+++ t/lib/PgLogTest/Schema.pm
@@ -19,7 +19,6 @@ __PACKAGE__->load_namespaces;
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->load_components(qw/Schema::PgLog/);
 
-__PACKAGE__->load_namespaces(
-	default_resultset_class => "+DBIx::Class::ResultSet::PgLog" );
+#__PACKAGE__->load_namespaces(default_resultset_class => "+DBIx::Class::ResultSet::PgLog");
 
 1;
